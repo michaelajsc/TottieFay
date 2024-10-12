f18933fd901ce12596808c60d368e6713feef7fa
Michaela Chester <michaela_gould@hotmail.com>
2024-10-12 21:08:17 +0100

    header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logoImg from './assets/TOTTIEFAYART.png';
+import Header from './components/Header/Header.jsx';
+
+
+function App() {
+    return (
+        <div>
+          <Header />
+          <main>
+          <h1>Tottie Fay</h1>
+          </main>
+        </div>
+      );
+    }
